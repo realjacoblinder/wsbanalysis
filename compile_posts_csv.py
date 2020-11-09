2b5f833b1404eb9dcb66967a6062bd564c993fca
SlowRoastedRibs <jl@jacoblinder.net>
2020-11-09 15:02:37 -0500

    updated json to csv conversion with coversion to EST

--- compile_posts_csv.py
+++ compile_posts_csv.py
@@ -3,6 +3,11 @@ import os
 from tqdm import tqdm
 import pandas as pd
 
+def convert_to_est(gmt_timestamp):
+	return int(gmt_timestamp)-(60*60*5)
+	
+
+
 i = 0
 
 for file in tqdm(os.scandir('post_data')):
@@ -13,6 +18,7 @@ for file in tqdm(os.scandir('post_data')):
             data = data + json.load(f)
     i += 1
 export = pd.DataFrame(data)
+export['created_utc'] = export['created_utc'].apply(convert_to_est)
 to_keep = ['author', 'created_utc', 'title', 'link_flair_text','selftext']
 export = export[to_keep]
 export.to_csv('all_good_posts.csv')
